07e7fd0362a3ea5fd9b9e9e8da2fca89942bce3d
Eric Ross <eric.ross@hp.com>
2016-06-16 14:06:42 -0700

    salqe base

---
+++ salqe/files/proxy.sh
@@ -0,0 +1,9 @@
+export http_proxy=http://web-proxy.austin.hpicorp.net:8080
+export https_proxy=$http_proxy
+export ftp_proxy=$http_proxy
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$http_proxy
+export FTP_PROXY=$http_proxy
+export no_proxy=127.0.0.1,localhost,.hp.com,*.hp.com,.hpicorp.net,*.hpicorp.net,15.0.0.0/9,.vcd.hp.com,*.vcd.hp.com,.vcs.rd.hpicorp.net,*.vcs.rd.hpicorp.net
+export NO_PROXY=$no_proxy
+
